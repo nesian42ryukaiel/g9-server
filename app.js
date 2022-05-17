b1b74b713dac3231b51c23c59e0543e3c303345b
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-05-17 18:38:49 +0900

    more buildup

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 import express from "express";
 import cors from "cors";
+import multer from "multer";
+import Base64 from "./plugins/Base64.js"; // nodemon seems to accept only if ".js" is appended...
 const app = express();
 const port = 3000;
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const date = new Date();
+    const year = date.getUTCFullYear();
+    const month = date.getUTCMonth() + 1;
+    const day = date.getUTCDate();
+    const varpath = `${year}/${(month < 10 ? "0" : "") + month}/${
+      (day < 10 ? "0" : "") + day
+    }/`;
+    if (!fs.existsSync("./image/" + varpath)) {
+      console.log("\n----------------\n");
+      console.log(
+        "\ncreating new directory...\n" + "./image/" + varpath + "\n"
+      );
+      console.log("\n----------------\n");
+      fs.mkdirSync("./image/" + varpath, { recursive: true });
+    }
+    cb(null, "./image/" + varpath); // for some reason this doesn't work
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  console.log("\n----------------\n");
+  console.log("Home request; the G9 server does not accept such command ;)");
+  console.log("\n----------------\n");
+  res.send("Temporary page for home request; will be deleted later on");
 });
 
 app.listen(port, () => {
-  console.log(`G9 server listening on port ${port}`);
+  console.log("\n----------------\n");
+  console.log(`G9 server app listening at http://localhost:${port}`);
+  console.log("\n----------------\n");
 });
