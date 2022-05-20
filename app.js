ed281ab68c26e6040b99e9a4f819bab6cc885a6e
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-05-20 16:56:17 +0900

    in ES6 you need to specify __dirname

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const corsOptions = {
   origin: ["http://localhost:9090"],
   credentials: true,
 };
+const __dirname = path.resolve();
 const port = 3000;
 const thisServer = `http://127.0.0.1:${port}`;
 const storage = multer.diskStorage({
